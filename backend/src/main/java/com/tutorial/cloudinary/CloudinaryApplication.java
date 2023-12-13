2e3ec514d5dd032590430e1309626f26da9eeb23
jhordyess <dev@jhordyess.com>
2023-12-12 23:43:01 -0400

    Añadido configuración de CORS

--- backend/src/main/java/com/tutorial/cloudinary/CloudinaryApplication.java
+++ backend/src/main/java/com/tutorial/cloudinary/CloudinaryApplication.java
@@ -3,6 +3,11 @@ package com.tutorial.cloudinary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Configuring CORS
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class CloudinaryApplication {
 
@@ -10,4 +15,18 @@ public class CloudinaryApplication {
 		SpringApplication.run(CloudinaryApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("DELETE", "GET", "POST", "PUT")
+						.allowedHeaders("*")
+						.allowCredentials(false)
+						.maxAge(3600);
+			}
+		};
+	}
 }
